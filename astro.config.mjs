9cca2b62fcdab63dcd21a7dc7acf3d26aeaa2854
Jake Runzer <jakerunzer@gmail.com>
2024-02-26 21:06:15 -0500

    images and better themes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, squooshImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
@@ -8,6 +8,9 @@ export default defineConfig({
   integrations: [tailwind(), mdx()],
   prefetch: {
     prefetchAll: true,
+  },
+  image: {
+    service: squooshImageService(),
   },
   markdown: {
     shikiConfig: {
